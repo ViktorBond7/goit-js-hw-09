344c1096ee66fa2c67fae6f5fe635b5826526bc0
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2023-12-18 17:40:41 +0100

    update18

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -1,40 +1,126 @@
-//<form class="feedback-form" autocomplete="off">
-//   <label>
-//     Email
-//     <input type="email" name="email" autofocus />
-//   </label>
-//   <label>
-//     Message
-//     <textarea name="message" rows="8"></textarea>
-//   </label>
-//   <button type="submit">Submit</button>
-// </form>;
+// const FEEDBACK_MESSAGE_KEY = 'feedback-message';
 
-'feedback-form-state';
+// const form = document.querySelector('.feedback-form');
+// const input = form.querySelector('input');
+// const textarea = form.querySelector('textarea');
+
+// const initialMessage = localStorage.getItem(FEEDBACK_MESSAGE_KEY);
+// const initialMeil = localStorage.getItem(FEEDBACK_MAIL_KEY);
+
+// if (initialMessage) {
+//   textarea.value = initialMessage;
+// }
+
+// form.addEventListener('submit', event => {
+//   event.preventDefault();
+
+//   const emailValue = event.target.email.value.trim();
+//   const passwordlValue = event.target.message.value.trim();
+//   if (!emailValue || !passwordlValue) {
+//     alert('All form fields must be filled in');
+//     return;
+//   }
+
+// try {
+//   const initialFormdata = localStorage.getItem(FEEDBACK_MESSAGE_KEY);
+//   form.elements.forEach(element => {
+//     const storageValue = initialFormdata[element.name];
+//     if (storageValue) {
+//       element.value = storageValue;
+//     }
+//   });
+// } catch (e) {
+//   console.error('error');
+// }
+
+//   const formData = new FormData(form);
+//   const formObject = {};
+//   formData.forEach((value, key) => {
+//     formObject[key] = value;
+//     localStorage.removeItem(FEEDBACK_MESSAGE_KEY);
+//     form.reset();
+//   });
+//   console.log(formObject);
+// });
+
+// form.addEventListener('input', event => {
+//   const message = event.target.value;
+//   localStorage.setItem(FEEDBACK_MESSAGE_KEY, message);
+// });
+
+// input.addEventListener('input', event => {
+//   const email = event.target.value;
+//   localStorage.setItem(FEEDBACK_MESSAGE_KEY, email);
+// });
+
 const FEEDBACK_DATA_KEY = 'feedback';
-const FEEDBACK_MESSAGE_KEY = 'feedback-message';
 
 const form = document.querySelector('.feedback-form');
-const input = form.querySelector('input');
-const textarea = form.querySelector('textarea');
+// const textarea = form.querySelector('textarea');
 
-const initialMessage = localStorage.getItem(FEEDBACK_MESSAGE_KEY);
+// const myLocalStorage = {
+//   get: (key, json = false) => {
+//     const data = localStorage.getItem(key);
 
-if (initialMessage) {
-  textarea.value = initialMessage;
+//     if (!json) return data;
+
+//     try {
+//       return JSON.parse(data);
+//     } catch (e) {
+//       console.error(e);
+//     }
+//   },
+//   set: (key, data, json = false) => {
+//     localStorage.setItem(key, json ? JSON.stringify(data) : data);
+//   },
+//   remove: key => {
+//     localStorage.removeItem(key);
+//   },
+// };
+
+try {
+  const initialFormData = JSON.parse(localStorage.getItem(FEEDBACK_DATA_KEY));
+
+  Array.from(form.elements).forEach(element => {
+    const storageValue = initialFormData[element.name];
+    if (storageValue) {
+      element.value = storageValue;
+    }
+  });
+} catch (e) {
+  console.error('PARSE STORAGE FORM ERROR');
 }
+
+form.addEventListener('input', () => {
+  const formData = new FormData(form);
+
+  const formObject = {};
+
+  formData.forEach((value, key) => {
+    formObject[key] = value;
+  });
+
+  localStorage.setItem(FEEDBACK_DATA_KEY, JSON.stringify(formObject));
+});
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-  localStorage.removeItem(FEEDBACK_MESSAGE_KEY);
+
+  const emailValue = event.target.email.value.trim();
+  const messageValue = event.target.message.value.trim();
+
+  if (!emailValue || !messageValue) {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formText = {
+    email: emailValue,
+    message: messageValue,
+  };
+  console.log(formText);
+
+  localStorage.removeItem(FEEDBACK_DATA_KEY);
 
   form.reset();
 });
-
-textarea.addEventListener(
-  'input',
-  debounce(event => {
-    const message = event.target.value;
-    localStorage.setItem(FEEDBACK_MESSAGE_KEY, message);
-  }, 250)
-);
